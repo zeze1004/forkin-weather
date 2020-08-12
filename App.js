fd89a733311c43c786599137dbb3bf02a8702025
zeze1004 <thwjd2717@naver.com>
2020-08-12 22:21:51 +0900

    사용자 위치 정보 수락 구문 추가

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import {Alert} from "react-native"
 //import { StyleSheet, Text, View } from 'react-native';
 import Loading from "./Loading"
 import * as Location from "expo-location"
@@ -9,8 +10,15 @@ import * as Location from "expo-location"
 
 export default class extends React.Component {
   getLocation = async() => {
-    const location = await Location.getCurrentPositionAsync()
-    console.log(location)
+    try {
+      const reponse = await Location.requestPermissionsAsync()
+      console.log(response)
+      const location = await Location.getCurrentPositionAsync()
+      console.log(location)
+    } catch (error) {
+      Alert.alert("can't find you")
+    }
+        
   }
   componentDidMount(){
     this.getLocation()
