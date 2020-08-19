eddcdfb82126d4ab971e588876da2ac1ab193ece
zeze1004 <thwjd2717@naver.com>
2020-08-20 00:02:19 +0900

    2.3 강의까지! 근데 에러 수정 못함 ...ㅋ

--- Weather.js
+++ Weather.js
@@ -1,11 +1,72 @@
 import React from 'react';
-import { View, Text, StyleSheet }from "react-native"
+import { View, Text, StyleSheet, StatusBar }from "react-native"
 import PropTypes from "prop-types"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+import{ LinearGradient } from 'expo-linear-gradient'
 
-export default function Weather({temp}){
-    return (<View style = {styles.container}>
-        <Text>{temp}</Text>
-        </View>
+const weatherOptions = {
+    Haze: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"]
+    },
+   
+    Thunderstorm: {
+        iconName:
+        gradient:
+    },
+    Drizzle: {
+        iconName:
+        gradient:
+    },
+    Rain: {
+        iconName:
+        gradient:
+    },
+    Snow: {
+        iconName:
+        gradient:
+    },
+    Atomsphere: {
+        iconName:
+        gradient:
+    },
+    Clear: {
+        iconName:
+        gradient:
+    },
+    Clouds: {
+        iconName:
+        gradient:
+    },
+    Mist: {
+        iconName:
+        gradient:
+    },
+    Dust: {
+        iconName:
+        gradient:
+    },
+}
+
+export default function Weather({temp,condition}){
+    return (
+        <LinearGradient
+          // Background Linear Gradient
+          colors={weatherOptions[condition].gradient}
+          style={styles.container}
+        >
+            <StatusBar barStyle="light-content" />
+        <View style = {styles.halfContainer}>
+            <MaterialCommunityIcons 
+                size={200} 
+                name= {weatherOptions[condition].iconName}
+                color="white"
+            />
+            <Text style={styles.temp}>{temp}도</Text>
+            <Text style={styles.zeze}>제제짱❤</Text>
+            </View>
+            <View style = {styles.halfContainer} />
+          </LinearGradient>
     )
 }
 
@@ -30,5 +91,18 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
+    },
+    temp: {
+        fontSize: 60,
+        color:"white"
+    },
+    halfContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    zeze: {
+        fontSize: 20,
+        color:"pink"
     }
 })
